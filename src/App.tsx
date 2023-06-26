f7e0514ef90578adf7047d3929297c20fc9f266a
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2023-06-27 01:30:28 +0200

    Add version information

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, IconButton } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Editor from "./Editor";
 
@@ -8,29 +8,55 @@ export enum Version {
   Master,
 }
 
+interface BackendVersions {
+  release: string;
+  master: string;
+}
+
 function App() {
   const [version, setVersion] = useState<Version | undefined>(undefined);
+  const [backendVersions, setBackendVersions] = useState<
+    BackendVersions | undefined
+  >(undefined);
+
+  useEffect(() => {
+    fetch("https://tomgroenwoldt.de/versions").then((res) =>
+      res.json().then(setBackendVersions)
+    );
+  });
+
   if (version === undefined) {
     return (
       <div id="main">
-        <Box id={"choice"}>
-          <Box
-            id={"choice-value"}
-            onClick={() => {
-              console.log("test");
-              setVersion(Version.Release);
-            }}
-          >
-            <img id={"logo"} src={process.env.PUBLIC_URL + "/logo.png"} />
-            <code>{"release 23.05"}</code>
+        <Box id={"center"}>
+          <Box id={"header"}>{"choose between latest release or master"}</Box>
+          <Box id={"choice"}>
+            <Box
+              id={"choice-value"}
+              onClick={() => {
+                setVersion(Version.Release);
+              }}
+            >
+              <img id={"logo"} src={process.env.PUBLIC_URL + "/logo.png"} />
+              {backendVersions === undefined ? (
+                <Spinner id={"spinner"} thickness={"3px"} />
+              ) : (
+                <code id={"spinner"}>{backendVersions.release}</code>
+              )}
+            </Box>
+            <Box id={"choice-value"} onClick={() => setVersion(Version.Master)}>
+              <img
+                id={"logo"}
+                src={process.env.PUBLIC_URL + "/github-mark-white.png"}
+              />
+              {backendVersions === undefined ? (
+                <Spinner id={"spinner"} thickness={"3px"} />
+              ) : (
+                <code id={"spinner"}>{backendVersions.master}</code>
+              )}
+            </Box>
           </Box>
-          <Box id={"choice-value"} onClick={() => setVersion(Version.Master)}>
-            <img
-              id={"logo"}
-              src={process.env.PUBLIC_URL + "/github-mark-white.png"}
-            />
-            <code>{"master 8d39a81"}</code>
-          </Box>
+          <Box id={"footer"}>{"updated daily at 00:00 UTC"}</Box>
         </Box>
       </div>
     );
