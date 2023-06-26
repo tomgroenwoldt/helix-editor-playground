98fa16cdb6de8aa5387bc3a1f0332e4bef5b3125
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2023-06-26 17:54:24 +0200

    Allow choice between master and release version of helix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import { Box, Button, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
 import "./App.css";
 import Editor from "./Editor";
 
+export enum Version {
+  Release,
+  Master,
+}
+
 function App() {
+  const [version, setVersion] = useState<Version | undefined>(undefined);
+  if (version === undefined) {
+    return (
+      <div id="main">
+        <Box id={"choice"}>
+          <Box
+            id={"choice-value"}
+            onClick={() => {
+              console.log("test");
+              setVersion(Version.Release);
+            }}
+          >
+            <img id={"logo"} src={process.env.PUBLIC_URL + "/logo.png"} />
+            <code>{"release 23.05"}</code>
+          </Box>
+          <Box id={"choice-value"} onClick={() => setVersion(Version.Master)}>
+            <img
+              id={"logo"}
+              src={process.env.PUBLIC_URL + "/github-mark-white.png"}
+            />
+            <code>{"master 8d39a81"}</code>
+          </Box>
+        </Box>
+      </div>
+    );
+  }
   return (
     <div id="main">
-      <Editor />
+      <Editor version={version} />
     </div>
   );
 }
--- src/Watcher.tsx
+++ src/Watcher.tsx
@@ -1,17 +1,11 @@
 import { ITerminalOptions, Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
 import "xterm/css/xterm.css";
 import "./Editor.css";
 import { Box } from "@chakra-ui/react";
 import { WebglAddon } from "xterm-addon-webgl";
 import { AttachAddon } from "./AttachAddon";
-import { Version } from "./App";
 
-interface EditorProps {
-  version: Version;
-}
-
-const Editor: React.FC<EditorProps> = (props: EditorProps) => {
+const Watcher: React.FC = () => {
   // Create xtermjs terminal.
   const terminalOptions: ITerminalOptions = {
     theme: { background: "#282a36" },
@@ -21,36 +15,18 @@ const Editor: React.FC<EditorProps> = (props: EditorProps) => {
   };
   const term = new Terminal(terminalOptions);
 
-  const version = props.version === Version.Release ? "release" : "master";
-
-  const ws = new WebSocket("wss://tomgroenwoldt.de/helix/" + version);
+  const ws = new WebSocket("ws://127.0.0.1:8080/watch/bebenulautus-dubonules");
   ws.addEventListener("open", (_event) => {
     // Load addons.
-    const fitAddon = new FitAddon();
-    term.loadAddon(fitAddon);
     const attachAddon = new AttachAddon(ws);
     term.loadAddon(attachAddon);
     term.loadAddon(new WebglAddon());
 
     term.open(document.getElementById("editor")!);
     term.focus();
-
-    window.addEventListener("resize", () => {
-      fitAddon.fit();
-    });
-    const encoder = new TextEncoder();
-    term.onResize((data) => {
-      ws.send(encoder.encode("\x01" + JSON.stringify(data)));
-    });
-    term.onData((data) => {
-      ws.send(encoder.encode("\x00" + data));
-    });
-
-    // Fit the terminal on first render.
-    fitAddon.fit();
   });
 
   return <Box id={"editor"} h="full"></Box>;
 };
 
-export default Editor;
+export default Watcher;
